0bc8b6d9f6344e8be30f2f0027dcf58f5b0dd1dd
SERGEI MLK <146926706+sergeimlk@users.noreply.github.com>
2023-12-18 16:15:42 +0100

    Update 2AKISER.py

--- AKIDASHBOARD/2AKISER.py
+++ AKIDASHBOARD/2AKISER.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="AKIGORA DASHBOARD", layout="wide", initial_sideba
 
 
 # Ajouter le logo dans la barre latérale et réduire la taille de 30%
-logo_path = r"AKIDASHBOARD/DATA AKIGORA/2LOGO.PNG"
+logo_path = r"AKIGORA/DATA AKIGORA/2LOGO.PNG"
 logo = Image.open(logo_path)
 logo_resized = logo.resize((int(logo.width * 1), int(logo.height * 1)))  # Réduire de 30%
 st.sidebar.image(logo_resized, use_column_width=True)
@@ -31,7 +31,7 @@ import remerciements
 
 # Charger les données
 #df = pd.read_csv(r'OneDrive/Documents/AKIGORA RAPPORT/DATA AKIGORA/AkiEXPERT.csv')
-df = pd.read_csv(r'AKIGORADASHBOARD/DATA AKIGORA/AkiEXPERT.csv')
+df = pd.read_csv(r'AKIGORA/DATA AKIGORA/AkiEXPERT.csv')
 
 st.sidebar.title("📊 AKIGORA DASHBOARD")
 
