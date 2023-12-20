a8086f5ca5ff0f146f75cabae8e227e8fdb38720
SERGEI MLK <146926706+sergeimlk@users.noreply.github.com>
2023-12-20 09:56:00 +0100

    Update cv.py

--- AKIDASHBOARD/cv.py
+++ AKIDASHBOARD/cv.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def run():
     # Afficher l'image
-    st.image('AKIGORA/DATA AKIGORA/CV.png')
+    st.image('DATA AKIGORA/1LOGO.png')
 
 # Vérifiez si le script est exécuté directement (non importé en tant que module)
 if __name__ == "__main__":
