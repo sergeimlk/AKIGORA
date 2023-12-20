7a40394d276b7c4a95b8ec526da36b3f27f47c3b
SERGEI MLK <146926706+sergeimlk@users.noreply.github.com>
2023-12-20 10:41:45 +0100

    Update cv.py

--- AKIDASHBOARD/cv.py
+++ AKIDASHBOARD/cv.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def run():
     # Afficher l'image directement avec st.image()
-    st.image('AKIGORA/DATA AKIGORA/1LOGO.png')
+    st.image('AKIGORA/DATA AKIGORA/CV.png')
 
 # Vérifiez si le script est exécuté directement (non importé en tant que module)
 if __name__ == "__main__":
