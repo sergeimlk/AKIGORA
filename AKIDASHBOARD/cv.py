52ba5323320dd1697cd4382b6b530a2499db384c
SERGEI MLK <146926706+sergeimlk@users.noreply.github.com>
2023-12-20 09:35:03 +0100

    Update cv.py

--- AKIDASHBOARD/cv.py
+++ AKIDASHBOARD/cv.py
@@ -5,7 +5,7 @@ import json
 
 def run():
     # Charger le DataFrame
-    df = pd.read_pdf('AKIGORA/DATA AKIGORA/CV.pdf')
+    df = pd.read_png('AKIGORA/DATA AKIGORA/CV.png')
 
 # Vérifiez si le script est exécuté directement (non importé en tant que module)
 if __name__ == "__main__":
