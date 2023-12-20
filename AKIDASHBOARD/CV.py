0ed075d8f7e3c75533beae19c9bd1a72f67b0749
SERGEI MLK <146926706+sergeimlk@users.noreply.github.com>
2023-12-20 09:16:45 +0100

    Update CV.py

--- AKIDASHBOARD/CV.py
+++ AKIDASHBOARD/CV.py
@@ -5,47 +5,7 @@ import json
 
 def run():
     # Charger le DataFrame
-    df = pd.read_csv('OneDrive/Documents/AKIGORA RAPPORT/DATA AKIGORA/AkiEXPERT.csv')
-
-    # Fonction pour extraire les coordonnées
-    def extract_coordinates(geo):
-        try:
-            geo_data = json.loads(geo)
-            lat, long = geo_data['location']['coordinates']
-            return lat, long
-        except:
-            return None, None
-
-    # Appliquer la fonction à chaque ligne du DataFrame
-    df[['latitude', 'longitude']] = df['geo'].apply(extract_coordinates).apply(pd.Series)
-
-    # Inverser latitude et longitude
-    df[['latitude', 'longitude']] = df[['longitude', 'latitude']]
-
-    # Afficher le DataFrame résultant
-    st.dataframe(df[['latitude', 'longitude']])
-
-    # Afficher la carte avec Pydeck
-    st.pydeck_chart(pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=df['latitude'].mean(),
-            longitude=df['longitude'].mean(),
-            zoom=5,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=df,
-                get_position='[longitude, latitude]',
-                get_radius=2000,
-                get_color='[200, 30, 0, 160]',
-                get_elevation='quantity',  # Ajouter cette ligne pour la dimension 3D
-                pickable=True,
-            ),
-        ],
-    ))
+    df = pd.read_pdf('AKIGORA/AKIDASHBOARD/DATA/CV.pdf')
 
 # Vérifiez si le script est exécuté directement (non importé en tant que module)
 if __name__ == "__main__":
