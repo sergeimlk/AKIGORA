2a3cb0e711c0f7a1f8bd2b775873d41f7a2f8b89
SERGEI MLK <146926706+sergeimlk@users.noreply.github.com>
2023-12-20 10:07:06 +0100

    Update cv.py

--- AKIDASHBOARD/cv.py
+++ AKIDASHBOARD/cv.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
 
 def run():
-    df = pd.read_image('AKIGORA/DATA AKIGORA/1LOGO.png')
-
+    # Afficher l'image directement avec st.image()
+    st.image('AKIGORA/DATA AKIGORA/1LOGO.png')
 
 # Vérifiez si le script est exécuté directement (non importé en tant que module)
 if __name__ == "__main__":
