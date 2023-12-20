09f3e88ecfd7d6d2f7d9e9b97a8a41c41d065874
SERGEI MLK <146926706+sergeimlk@users.noreply.github.com>
2023-12-20 14:40:15 +0100

    Update 2AKISER.py

--- AKIDASHBOARD/2AKISER.py
+++ AKIDASHBOARD/2AKISER.py
@@ -42,7 +42,7 @@ def run():
                 get_position='[longitude, latitude]',
                 radius=2000,
                 elevation_scale=4,
-                elevation_range=[0, 10000],
+                elevation_range=[0, 50000],
                 pickable=True,
                 extruded=True,
             ),
